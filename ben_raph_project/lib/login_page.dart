6ef236928794a7b019726926ff2b0122364bb178
RaphSchp <135048201+RaphSchp@users.noreply.github.com>
2023-10-15 17:19:10 +0200

    add image start code

---
+++ ben_raph_project/lib/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage("assets/images/bg.png"))),
+    );
+  }
+}
